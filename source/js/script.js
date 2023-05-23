add51fa4f8433c159da1670db53897e1cd5fd54d
Timkoo <bfg20k@mail.ru>
2023-05-23 22:05:22 +0300

    добавлена стилизация мелких сеток

---
+++ source/js/script.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector('.site-header__menu-button');
+const menu = document.querySelector('.site-header__navigation');
+menu.classList.remove('site-header__navigation--nojs');
+
+menuButton.addEventListener('click', () => {
+  menu.classList.contains('site-header__navigation--visible') ?
+  menu.classList.remove('site-header__navigation--visible') :
+  menu.classList.add('site-header__navigation--visible');
+})
