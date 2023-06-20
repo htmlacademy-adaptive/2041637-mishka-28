466e220f1a1eb3234670189098bb6d1a89bf0539
Timkoo <bfg20k@mail.ru>
2023-06-20 12:55:09 +0300

    доработаны декоративные элементы

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector('.site-header__menu-button');
 const menu = document.querySelector('.site-header__navigation');
 menu.classList.remove('site-header__navigation--nojs');
+menuButton.classList.remove('site-header__menu-button--nojs');
 
 menuButton.addEventListener('click', () => {
   menu.classList.contains('site-header__navigation--visible') ?
