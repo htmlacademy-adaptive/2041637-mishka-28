5b5f923375c7aadbe717cd69e63327837bbb5c0a
Timkoo <bfg20k@mail.ru>
2023-06-26 20:56:37 +0300

    доработана графика

--- source/js/script.js
+++ source/js/script.js
@@ -2,9 +2,26 @@ const menuButton = document.querySelector('.site-header__menu-button');
 const menu = document.querySelector('.site-header__navigation');
 menu.classList.remove('site-header__navigation--nojs');
 menuButton.classList.remove('site-header__menu-button--nojs');
+const cartButtons = document.querySelectorAll('.product-card__cart');
+const weeklyButton = document.querySelector('.weekly__button');
+const cartButtonsList = [...cartButtons, weeklyButton];
+const modal = document.querySelector('.modal');
+const modalButton = modal.querySelector('.modal__button');
 
 menuButton.addEventListener('click', () => {
   menu.classList.contains('site-header__navigation--visible') ?
   menu.classList.remove('site-header__navigation--visible') :
   menu.classList.add('site-header__navigation--visible');
+});
+
+cartButtonsList.forEach((button) => {
+  button.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    modal.classList.add('modal--opened');
+  })
+})
+
+modalButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  modal.classList.remove('modal--opened');
 })
